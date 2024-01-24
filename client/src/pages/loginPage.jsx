260cd43ce664e4adbd624ae6f5b0bcece0f94cf6
Gireesh-KL <gireeshkl257@gmail.com>
2024-01-24 19:52:53 +0530

    before 8

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function LoginPage() {
   return(
-    <div>
-        
+    <div className='mt-4'>
+    <h1 className='text-4xl text-center mb-4'>Login</h1>
+        <form className='max-w-md mx-auto'>
+           <input type="email"  placeholder="johndoe@gmailc.com" className='w-full border my-1 py-2 px-3 rounded-2xl'/>
+            <input type="password" placeholder='password' className='w-full border my-1 py-2 px-3 rounded-2xl'/>
+            <button className='bg-[#F5385D] w-full p-2 text-white rounded-2xl'>Login</button>
+        </form>
     </div>
   )
 }
